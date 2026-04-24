70c6c46183658a5d6dfcdfd9d051ef9c344ecc3c
minyoad <minyoad@gmail.com>
2026-04-24 15:43:46 +0800

    Add initialization checks to SP utility methods

--- core/data/src/main/java/top/yogiczy/mytv/core/data/utils/SP.kt
+++ core/data/src/main/java/top/yogiczy/mytv/core/data/utils/SP.kt
@@ -21,30 +21,56 @@ object SP {
             return op(key, defValue)
         } catch (ex: Exception) {
             log.e("SP", ex)
-            sp.edit().remove(key).apply()
+            if (::sp.isInitialized) {
+                sp.edit().remove(key).apply()
+            }
             return defValue
         }
     }
 
-    fun getString(key: String, defValue: String) = safeGet(key, defValue, sp::getString)!!
-    fun putString(key: String, value: String) = sp.edit().putString(key, value).apply()
+    fun getString(key: String, defValue: String) = 
+        if (::sp.isInitialized) safeGet(key, defValue, sp::getString)!! else defValue
+    
+    fun putString(key: String, value: String) { 
+        if (::sp.isInitialized) sp.edit().putString(key, value).apply() 
+    }
 
     fun getStringSet(key: String, defValue: Set<String>): Set<String> =
-        safeGet(key, defValue, sp::getStringSet)!!
+        if (::sp.isInitialized) safeGet(key, defValue, sp::getStringSet)!! else defValue
 
-    fun putStringSet(key: String, value: Set<String>) = sp.edit().putStringSet(key, value).apply()
+    fun putStringSet(key: String, value: Set<String>) { 
+        if (::sp.isInitialized) sp.edit().putStringSet(key, value).apply() 
+    }
 
-    fun getInt(key: String, defValue: Int) = safeGet(key, defValue, sp::getInt)
-    fun putInt(key: String, value: Int) = sp.edit().putInt(key, value).apply()
+    fun getInt(key: String, defValue: Int) = 
+        if (::sp.isInitialized) safeGet(key, defValue, sp::getInt) else defValue
+        
+    fun putInt(key: String, value: Int) { 
+        if (::sp.isInitialized) sp.edit().putInt(key, value).apply() 
+    }
 
-    fun getLong(key: String, defValue: Long) = safeGet(key, defValue, sp::getLong)
-    fun putLong(key: String, value: Long) = sp.edit().putLong(key, value).apply()
+    fun getLong(key: String, defValue: Long) = 
+        if (::sp.isInitialized) safeGet(key, defValue, sp::getLong) else defValue
+        
+    fun putLong(key: String, value: Long) { 
+        if (::sp.isInitialized) sp.edit().putLong(key, value).apply() 
+    }
 
-    fun getFloat(key: String, defValue: Float) = safeGet(key, defValue, sp::getFloat)
-    fun putFloat(key: String, value: Float) = sp.edit().putFloat(key, value).apply()
+    fun getFloat(key: String, defValue: Float) = 
+        if (::sp.isInitialized) safeGet(key, defValue, sp::getFloat) else defValue
+        
+    fun putFloat(key: String, value: Float) { 
+        if (::sp.isInitialized) sp.edit().putFloat(key, value).apply() 
+    }
 
-    fun getBoolean(key: String, defValue: Boolean) = safeGet(key, defValue, sp::getBoolean)
-    fun putBoolean(key: String, value: Boolean) = sp.edit().putBoolean(key, value).apply()
+    fun getBoolean(key: String, defValue: Boolean) = 
+        if (::sp.isInitialized) safeGet(key, defValue, sp::getBoolean) else defValue
+        
+    fun putBoolean(key: String, value: Boolean) { 
+        if (::sp.isInitialized) sp.edit().putBoolean(key, value).apply() 
+    }
 
-    fun clear() = sp.edit().clear().apply()
+    fun clear() { 
+        if (::sp.isInitialized) sp.edit().clear().apply() 
+    }
 }
