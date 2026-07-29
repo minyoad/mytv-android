30ce23d72a659b7ec1dff9da41e4b558c3d4b023
mingyang wu <minyoad@gmail.com>
2026-07-29 18:13:03 +0800

    chore(release): bump version to 2.4.5-beta (code 15)

--- tv/build.gradle.kts
+++ tv/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "top.yogiczy.mytv.tv"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 14
-        versionName = "2.4.4-beta"
+        versionCode = 15
+        versionName = "2.4.5-beta"
         vectorDrawables {
             useSupportLibrary = true
         }
