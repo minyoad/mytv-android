8e804284a823b2c11a47653c71f621c1cd7a7911
mingyang wu <minyoad@gmail.com>
2026-09-01 14:51:50 +0800

    chore(release): bump version to 2.5.2 (code 22)

--- tv/build.gradle.kts
+++ tv/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "top.yogiczy.mytv.tv"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 21
-        versionName = "2.5.1-beta"
+        versionCode = 22
+        versionName = "2.5.2"
         vectorDrawables {
             useSupportLibrary = true
         }
