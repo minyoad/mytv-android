631ebbd8441a1a8abb89b90743fbf25d5ef0d23e
mingyang wu <mingyang.wu@tpv-tech.com>
2026-09-11 00:13:00 +0800

    chore(release): bump version to 2.6.0-beta2 (code 26)

--- tv/build.gradle.kts
+++ tv/build.gradle.kts
@@ -42,8 +42,8 @@ android {
         applicationId = "top.yogiczy.mytv.tv"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 25
-        versionName = "2.6.0-beta1"
+        versionCode = 26
+        versionName = "2.6.0-beta2"
         vectorDrawables {
             useSupportLibrary = true
         }
